9ce51d4baac5b1b8b595f6273d55e4382105cc8e
gerich333 <gerich333@yandex.ru>
2023-08-20 23:36:34 +0700

    create new table "Basket"

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -13,3 +13,19 @@ create table Family (
 	,SurName varchar(50)
 	,BudgetValue money
 );
+
+
+create table Basket (
+	ID int identity(1,1) Primary key
+	,ID_SKU int not null
+	,ID_Family int not null
+	,Quantity int 
+	,Value int
+	,PurchaseDate date default CONVERT (date,GETDATE())
+	,DiscountValue smallmoney check (DiscountValue >= 0)
+
+	,CONSTRAINT check_Q_and_V CHECK (Quantity>=0 AND Value>=0)
+
+	,foreign key (ID_SKU) references dbo.SKU(ID) on update cascade on delete cascade
+	,foreign key (ID_Family) references dbo.Family(ID) on update cascade on delete cascade
+);
